5bd4068a9703e38b909e1394d1c795aafb98fb1b
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2024-05-31 22:29:26 +0600

    1st part added

--- src/Shared/Footer/Footer.jsx
+++ src/Shared/Footer/Footer.jsx
@@ -2,9 +2,25 @@
 
 const Footer = () => {
     return (
-        <div>
-            
-        </div>
+        <>
+        <footer className="footer p-10 bg-base-300 text-base-content">
+  <nav>
+    <h6 className="footer-title">Services</h6> 
+    <a className="link link-hover">Branding</a>
+    <a className="link link-hover">Design</a>
+    <a className="link link-hover">Marketing</a>
+    <a className="link link-hover">Advertisement</a>
+  </nav> 
+  <nav>
+    <h6 className="footer-title">Company</h6> 
+    <a className="link link-hover">About us</a>
+    <a className="link link-hover">Contact</a>
+    <a className="link link-hover">Jobs</a>
+    <a className="link link-hover">Press kit</a>
+  </nav> 
+  
+</footer>
+        </>
     );
 };
 
