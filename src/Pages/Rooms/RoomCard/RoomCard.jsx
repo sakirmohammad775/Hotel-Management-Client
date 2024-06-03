4b52cca55fe584e560d1bb3b111ee68888431430
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2024-06-03 10:38:01 +0600

    added the carousel,cardItem

--- src/Pages/Rooms/RoomCard/RoomCard.jsx
+++ src/Pages/Rooms/RoomCard/RoomCard.jsx
@@ -13,7 +13,7 @@ const RoomCard = () => {
                         className="h-full w-full object-cover"
                     />
                 </CardHeader>
-                
+
                 <CardBody>
                     <Typography variant="h6" color="gray" className="mb-4 uppercase">
                         startups</Typography>
@@ -25,24 +25,11 @@ const RoomCard = () => {
                         transition. It was until recently a traditional boxed software company
                         selling licenses. Yet its own business model disruption is only part
                         of the story
-                    </Typography><a href="#" className="inline-block">
-                        <Button variant="text" className="flex items-center gap-2">
-                            Learn More
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                stroke="currentColor"
-                                strokeWidth={2}
-                                className="h-4 w-4"
-                            >
-                                <path
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3"
-                                />
-                            </svg>
-                        </Button> </a>
+                    </Typography>
+                    <Button variant="text" className="flex items-center gap-2">
+                        Learn More
+
+                    </Button>
                 </CardBody>
             </Card>
         </>
