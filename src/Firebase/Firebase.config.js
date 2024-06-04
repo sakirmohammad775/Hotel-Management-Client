181f02a5f222ddcf8382dde82526eec517ceba52
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2024-06-04 23:31:34 +0600

    authprovider added

--- src/Firebase/Firebase.config.js
+++ src/Firebase/Firebase.config.js
@@ -14,5 +14,4 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-export default app
+export const app = initializeApp(firebaseConfig);
--- src/Provider/AuthProvider.jsx
+++ src/Provider/AuthProvider.jsx
@@ -1,8 +1,49 @@
+import { createContext, useEffect, useState } from "react";
+import { app } from "../Firebase/Firebase.config";
+import { getAuth, createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword } from "firebase/auth";
+
+export const AuthContext = createContext(null)
+const auth = getAuth(app)
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null)
+  const [loading, setLoading] = useState(true)
+
+  const createUser = (email, password) => {
+    setLoading(true)
+    return createUserWithEmailAndPassword(auth, email, password)
+  }
+  const signIn = (email, password) => {
+    setLoading(true);
+    return signInWithEmailAndPassword(auth, email, password);
+  };
 
 
-export const AuthProvider = () => {
-    return (
-      <></>
-    );
+  useEffect(() => {
+    const unSubscribe = onAuthStateChanged(auth, currentUser => {
+      setUser(currentUser)
+      console.log('current User:', currentUser);
+      setLoading(false)
+    })
+    return () => {
+      return unSubscribe() //clean up the subscription
+    }
+  }, [])
+
+  const authInfo = {
+    user,
+    loading,
+    createUser,
+    signIn
+
+  }
+  return (
+
+    <>
+      <AuthContext.Provider value={authInfo}>
+        {children}
+      </AuthContext.Provider>
+    </>
+  );
 };
 
