bf84a69d2c48169dd9b6e3ef8767dae62c1e74fa
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2024-06-10 10:30:33 +0600

    booking Row created

---
+++ src/Pages/MyBookings/BookingRow.jsx
@@ -0,0 +1,41 @@
+
+
+const BookingRow = ({booking}) => {
+    const {customerName,email}=booking
+    return (
+        <>
+            <tr>
+                <th>
+                    <label>
+                        <input type="checkbox" className="checkbox" />
+                    </label>
+                </th>
+                <td>
+                    <div className="flex items-center gap-3">
+                        <div className="avatar">
+                            <div className="mask mask-squircle w-12 h-12">
+                                <img src="https://img.daisyui.com/tailwind-css-component-profile-2@56w.png" alt="Avatar Tailwind CSS Component" />
+                            </div>
+                        </div>
+                        <div>
+                            <div className="font-bold">{email}</div>
+                            <div className="text-sm opacity-50">{customerName}</div>
+                        </div>
+                    </div>
+                </td>
+                <td>
+                    Zemlak, Daniel and Leannon
+                    <br />
+                    <span className="badge badge-ghost badge-sm">Desktop Support Technician</span>
+                </td>
+                <td>Purple</td>
+                <th>
+                    <button className="btn btn-ghost btn-xs">delete</button>
+                </th>
+            </tr>
+
+        </>
+    );
+};
+
+export default BookingRow;
--- src/Pages/MyBookings/MyBookings.jsx
+++ src/Pages/MyBookings/MyBookings.jsx
@@ -1,5 +1,6 @@
 import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../Provider/AuthProvider";
+import BookingRow from "./BookingRow";
 
 
 const MyBookings = () => {
@@ -33,37 +34,10 @@ const MyBookings = () => {
                         </tr>
                     </thead>
                     <tbody>
-                        {/* row 1 */}
-                        <tr>
-                            <th>
-                                <label>
-                                    <input type="checkbox" className="checkbox" />
-                                </label>
-                            </th>
-                            <td>
-                                <div className="flex items-center gap-3">
-                                    <div className="avatar">
-                                        <div className="mask mask-squircle w-12 h-12">
-                                            <img src="https://img.daisyui.com/tailwind-css-component-profile-2@56w.png" alt="Avatar Tailwind CSS Component" />
-                                        </div>
-                                    </div>
-                                    <div>
-                                        <div className="font-bold">{bookings.customerName}</div>
-                                        <div className="text-sm opacity-50">United States</div>
-                                    </div>
-                                </div>
-                            </td>
-                            <td>
-                                Zemlak, Daniel and Leannon
-                                <br />
-                                <span className="badge badge-ghost badge-sm">Desktop Support Technician</span>
-                            </td>
-                            <td>Purple</td>
-                            <th>
-                                <button className="btn btn-ghost btn-xs">delete</button>
-                            </th>
-                        </tr>
-                       
+                        
+                        {
+                            bookings.map(booking=><BookingRow key={booking._id} booking={booking}></BookingRow>)
+                        }
                     </tbody>
                
                 
