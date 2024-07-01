0cdcc61b12522df65ce742bc17dccf1cf468d54e
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2024-07-01 22:59:42 +0600

    added the p

--- src/Pages/Rooms/RoomCard/RoomCard.jsx
+++ src/Pages/Rooms/RoomCard/RoomCard.jsx
@@ -11,7 +11,7 @@ const RoomCard = ({ room }) => {
             <div className="card card-compact w-[340px] bg-base-100 shadow-xl">
                 <figure><img src={images} alt="Shoes" /></figure>
                 <div className="card-body">
-                   
+                    <h2 className="card-title">{room_description}</h2>
                     <p>Price per Night : {price}$</p>
                     <div className="card-actions justify-end">
                     <Link to={`/rooms/${_id}`}><button className="btn btn-primary">Details</button></Link>
